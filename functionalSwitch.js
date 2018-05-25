6417d9869c62cee268b50e309e4bbd994bf1699a
Lek Tin <lek-tin@users.noreply.github.com>
2018-05-25 15:10:08 +0800

    Update functionalSwitch.js

--- functionalSwitch.js
+++ functionalSwitch.js
@@ -1,10 +1,10 @@
 // Inspiration: https://hackernoon.com/rethinking-javascript-eliminate-the-switch-statement-for-better-code-5c81c044716d
-export functionSwitch = cases => defaultCase => key =>
+export funcSwitch = cases => defaultCase => key =>
   cases.hasOwnProperty(key) ? cases[key] : defaultCase 
 
 // Usage
 const switchColors = (value = 0, type) =>
-  switchcase({
+  funcSwitch({
     'GREEN': state + 10,
     'BLUE': state + 5,
     'ORANGE': state - 5,
